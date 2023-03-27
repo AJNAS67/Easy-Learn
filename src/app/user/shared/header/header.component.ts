ae2a8736c239cfdd64b6e12247f451ea5e77de87
ajnaskp <ajnaskp67@gmail.com>
2023-03-27 11:59:02 +0530

    header

--- src/app/user/shared/header/header.component.ts
+++ src/app/user/shared/header/header.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  userName!: string;
+  ngOnInit(): void {
+    
+  }
+  userLogened: boolean = true;
+  isOpen: boolean = false;
+  isNaveOpen: boolean = false;
+  proclick() {
+    if (this.isOpen === true) {
+      this.isOpen = false;
+      console.log(this.isOpen, 'is');
+    } else {
+      this.isOpen = true;
+      console.log(this.isOpen, 'fghbjnkm,');
+    }
+  }
+  clickNav() {
+    if (this.isNaveOpen === true) {
+      this.isNaveOpen = false;
+    } else {
+      this.isNaveOpen = true;
+    }
+  }
+  logout() {
+    localStorage.removeItem('access_token');
+  }
+  checkUser() {
+    return localStorage.getItem('access_token');
+  }
 
 }
