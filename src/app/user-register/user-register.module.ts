b6b84a040468d0705ad8164246a05be9471652a2
ajnaskp <ajnaskp67@gmail.com>
2023-04-04 06:39:35 +0530

    home page style fix

--- src/app/user-register/user-register.module.ts
+++ src/app/user-register/user-register.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterRoutingModule } from './user-register-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { SnackBarService } from './service/snack-bar.service';
+import { RegisterRoutingModule } from './user-register-routing.module';
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent, AdminLoginComponent],
@@ -29,6 +30,13 @@ import { SnackBarService } from './service/snack-bar.service';
     MatCardModule,
     MatSnackBarModule,
   ],
-  providers: [SnackBarService],
+  providers: [
+    SnackBarService,
+
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' },
+    },
+  ],
 })
 export class UserRegisterModule {}
