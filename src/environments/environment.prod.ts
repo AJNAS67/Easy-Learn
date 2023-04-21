209facca38f8e3f7a50625ae284e36b26570a282
ajnaskp <ajnaskp67@gmail.com>
2023-04-21 19:43:49 +0530

    fix for deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    apiUrl: 'http://localhost:3000',
+    appTitle: 'Easylearn',
+    CLIENT_ID:'ASD167d4t9LgsXIiJMYRkp2gwGfEMagwe2A_EFcBNi2yxYNXUcMq_GzzLSOCaitr48JZAl1g1Ei00hii',
+    socketUrl:'http://localhost:3000'
+  };
+  
