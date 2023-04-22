b4bfbaf7d4965e514ab33edbb833ef6a28eecefe
ajnaskp <ajnaskp67@gmail.com>
2023-04-22 14:31:06 +0530

    api.easylearn.store

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    apiUrl: 'http://localhost:3000',
+    apiUrl: 'https://api.easylearn.store',
     appTitle: 'Easylearn',
     CLIENT_ID:'ASD167d4t9LgsXIiJMYRkp2gwGfEMagwe2A_EFcBNi2yxYNXUcMq_GzzLSOCaitr48JZAl1g1Ei00hii',
-    socketUrl:'http://localhost:3000'
+    socketUrl:'https://api.easylearn.store'
   };
   
